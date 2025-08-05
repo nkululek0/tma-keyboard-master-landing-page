bf547ede7565ac3bce8416a31cb436dea07e6071
nkululek0 <nkululekovuyo0103@gmail.com>
2025-08-05 12:01:08 +0200

    (Update): creates and registers the 'LandingPage' component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { LandingPage } from '../LandingPage/LandingPage'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl'>Hello World</h1>
+      <LandingPage />
     </>
   )
 }
---
+++ src/LandingPage/LandingPage.jsx
@@ -0,0 +1,7 @@
+export function LandingPage () {
+    return (
+        <>
+            <h1 className='text-3xl'>Landing Page</h1>
+        </>
+    )
+}
